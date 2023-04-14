3a2e597f81daa70fd53cb4337461355faa7e707b
MikhaelVinicius <mickaelsoel@gmail.com>
2023-04-14 20:20:07 -0300

    Informações padrão de coordenadores criado

--- BackEnd/src/main/java/com/example/EditalProject/Entity/CoordenadorExtensao.java
+++ BackEnd/src/main/java/com/example/EditalProject/Entity/CoordenadorExtensao.java
@@ -1,5 +1,49 @@
 package com.example.EditalProject.Entity;
 
 public class CoordenadorExtensao extends User{
+    private String nome;
+    private String email;
+    private LocalDate dataNascimento;
+    private String cpf;
 
+    public CoordenadorExtensao(String nome, String email, LocalDate dataNascimento, String cpf) {
+        this.nome = nome;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+    }
+
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
