c1791ce75a89223811b40edc02845e59f1efe5c9
MikhaelVinicius <mickaelsoel@gmail.com>
2023-04-15 19:34:43 -0300

    Diretorio DAO e classe EditalExtensaoDAO criadas

---
+++ BackEnd/src/main/java/com/example/EditalProject/DAO/EditalExtensaoDAO.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class EditalExtensaoDAO {
+    public void insere(EditalExtensao edital) {
+        try (Connection conexao = ConnectionFactory.getConnection()) {
+            // Inserir os dados em uma tabela
+            PreparedStatement stmt = conexao.prepareStatement("INSERT INTO tabela_edital_extensao (titulo, descricao, prazo, criterios_selecao, requisitos) VALUES (?, ?, ?, ?, ?)");
+            stmt.setString(1, edital.getTitulo());
+            stmt.setString(2, edital.getDescricao());
+            stmt.setDate(3, java.sql.Date.valueOf(edital.getPrazo()));
+            stmt.setString(4, edital.getCriteriosSelecao());
+            stmt.setString(5, edital.getRequisitos());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar com o banco de dados: " + e.getMessage());
+        }
+    }
+}
