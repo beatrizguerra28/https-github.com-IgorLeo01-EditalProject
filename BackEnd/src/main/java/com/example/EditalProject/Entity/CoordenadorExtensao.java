700b4f7fbc53cdbb661657983841b4487adf9916
MikhaelVinicius <mickaelsoel@gmail.com>
2023-04-14 22:41:28 -0300

    Classes apagadas para correção, database mantido

--- BackEnd/src/main/java/com/example/EditalProject/Entity/CoordenadorExtensao.java
+++ BackEnd/src/main/java/com/example/EditalProject/Entity/CoordenadorExtensao.java
@@ -1,49 +1,4 @@
 package com.example.EditalProject.Entity;
 
 public class CoordenadorExtensao extends User {
-    private String titulo;
-    private String descricao;
-    private LocalDate prazo;
-    private String criteriosSelecao;
-    private String requisitos;
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setPrazo(LocalDate prazo) {
-        this.prazo = prazo;
-    }
-
-    public LocalDate getPrazo() {
-        return prazo;
-    }
-
-    public void setCriteriosSelecao(String criteriosSelecao) {
-        this.criteriosSelecao = criteriosSelecao;
-    }
-
-    public String getCriteriosSelecao() {
-        return criteriosSelecao;
-    }
-
-    public void setRequisitos(String requisitos) {
-        this.requisitos = requisitos;
-    }
-
-    public String getRequisitos() {
-        return requisitos;
-    }
 }
